00a87c1a464adb6672d699899d350ae92082cbd8
Rafael de Jeus Gebim <rafaelgebim@gmail.com>
2021-12-03 12:59:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+var lista = []
+var input_valor = document.getElementById("input_valor");
+
+input_valor.addEventListener('keyup', (ev) => {
+    if (ev.key === "Enter") {
+        addValue();
+    }
+})
+
+function addValue() {
+    var numberInput = input_valor.value
+    lista.push(Number(numberInput))
+
+    input_valor.value = ''
+    document.getElementById("lbl_qtnitens").innerHTML = lista.length
+}
+
+
+function Total() {
+    var total = 0
+    lista.forEach(valor => {
+        total += valor
+    });
+    document.getElementById('lbl_conta').innerHTML = total.toFixed(2).replace('.', ',')
+}
